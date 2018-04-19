59143c48055ed40b730cdda955bad5db8b9e77dc
tuzhili <834322062@qq.com>
2018-04-19 10:17:25 +0800

    2222

---
+++ src/main/java/com/example/demo/web/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.demo.web;
+
+/**
+ * @Title: 2222
+ * @Description:
+ * @Author: tuzhili
+ * @Date: 2018/4/19 10:11
+ * @Version:1.1.0
+ * @Copyright:Copyright (c) 浙江蘑菇加电子商务有限公司 2017 ~ 2018 版权所有
+ */
+public class HelloController {
+    String str = "1111";
+}
